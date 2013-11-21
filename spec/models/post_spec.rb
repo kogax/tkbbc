7b4a33efcc12a8178a8b70e100a69e2df5991f8e
kogax <shunsuke.koga@gmail.com>
2013-11-21 14:20:24 +0900

    add gem: spring, guard

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -6,12 +6,12 @@ describe Post do
   describe "#create" do
     let(:long_string_1001) {'a'*1001}
 
-    it "should fill body text" do
+    it "should have body text" do
       Post.create(body: 'hoge')
       expect(Post.find(1).body.present?).to be_true
     end
 
-    it "should not be valid" do
+    it "should not be valid with body(more than 1000 characters)" do
       @post = Post.new(body: long_string_1001)
       expect(@post).not_to be_valid
     end
